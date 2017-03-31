f086a035780261cfe13f8e4d321f490c0e177a68
Richard <richardhaddad@hotmail.fr>
2017-03-31 15:43:35 +0200

    Correction du problème des hobbys.

--- web/js/script.js
+++ web/js/script.js
@@ -1,6 +1,9 @@
 
 window.onload = onLoad;
 
+var GLOBALS = {
+	hobby_index: -1
+};
 
 function onLoad() {
 
@@ -17,32 +20,40 @@ function onLoad() {
 			butClicked($(form).find('.but[type="submit"]'));
 		}
 	});
-	
+
 	$('#ip_hobbys').keypress(e => {
-		if(e.key !== 'Enter') {
+		if (e.key !== 'Enter') {
 			return;
 		}
 		e.preventDefault();
-		
+
 		let i = e.target;
 		let val = i.value;
-		if(!val || !i.checkValidity()) {
+		if (!val || !i.checkValidity()) {
 			return;
 		}
-		
+
 		newHobby(val);
 		e.target.value = '';
 	});
-	
+
+	var hobbys = $('.hobbys').data('hobbys');
+
+	hobbys.forEach(h => newHobby(h));
+
 	setHobbyDeletable('.hobbys-item');
-	
+
 }
 
 function newHobby(value) {
-	let hobby = `<span class="hobbys-item tag deletable onhover">${value}<span class="delete mini-but glyphicon glyphicon-remove"></span></span>`;
-	
+	GLOBALS.hobby_index++;
+	let hobby = `<span class="hobbys-item tag deletable onhover">
+		${value}<span class="delete mini-but glyphicon glyphicon-remove"></span>
+		<input type="hidden" name="fos_user_registration_form[hobbies][${GLOBALS.hobby_index}][name]" value="${value}"/>
+	</span>`;
+
 	$('.hobbys .hobbys-content .hobbys-add').after(hobby);
-	
+
 	setHobbyDeletable($('.hobbys .hobbys-content .hobbys-item').first());
 }
 
