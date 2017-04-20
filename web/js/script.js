d19337ac841f4815cca8f6a5dd78d296118ef942
Richard <richardhaddad@hotmail.fr>
2017-04-20 02:42:56 +0200

    Avancement dans la vue (upload CV/Journal, page de modification/suppression, partie société).

--- web/js/script.js
+++ web/js/script.js
@@ -1,11 +1,9 @@
 
-/* global FORM */
+/* global FORM, FORM_NAME */
 
 window.onload = onLoad;
 
 var GLOBALS = {
-    hobby_index: -1,
-    skill_index: -1,
     education_index: -1,
     work_index: -1
 };
@@ -15,6 +13,8 @@ function onLoad() {
         GLOBALS.form = FORM;
     } catch (e) {
     }
+
+    bugFixes();
 
     function butClicked(but) {
         $(but).attr('disabled', true);
@@ -32,11 +32,17 @@ function onLoad() {
 
     if (GLOBALS.form) {
         initSkills();
-        initHobby();
+//        initHobby();
     }
 
     initEducations();
     initWork();
+}
+
+//Corrige les bugs recensés non corrigés de PHP
+
+function bugFixes() {
+    $('.field>input[type="hidden"]').parent().hide();
 }
 
 //General
@@ -47,9 +53,9 @@ function applyDate(str_type, str_date, str_val, index) {
     }
     let d = new Date(str_date);
 
-    $('#fos_user_' + GLOBALS.form + '_form_' + str_type + '_' + index + '_' + str_val + '_day option[value="' + d.getDate() + '"]').attr('selected', true);
-    $('#fos_user_' + GLOBALS.form + '_form_' + str_type + '_' + index + '_' + str_val + '_month option[value="' + (d.getMonth() + 1) + '"]').attr('selected', true);
-    $('#fos_user_' + GLOBALS.form + '_form_' + str_type + '_' + index + '_' + str_val + '_year option[value="' + d.getFullYear() + '"]').attr('selected', true);
+    $('#' + GLOBALS.form + '_' + str_type + '_' + index + '_' + str_val + '_day option[value="' + d.getDate() + '"]').attr('selected', true);
+    $('#' + GLOBALS.form + '_' + str_type + '_' + index + '_' + str_val + '_month option[value="' + (d.getMonth() + 1) + '"]').attr('selected', true);
+    $('#' + GLOBALS.form + '_' + str_type + '_' + index + '_' + str_val + '_year option[value="' + d.getFullYear() + '"]').attr('selected', true);
 }
 
 function setItemDeletable(item) {
@@ -60,83 +66,96 @@ function setItemDeletable(item) {
 
 function initSkills() {
 
-    $('#ip_skills').keypress(e => {
-        if (e.key !== 'Enter') {
-            return;
-        }
-        e.preventDefault();
-
-        let i = e.target;
-        let val = i.value;
-        if (!val || !i.checkValidity()) {
-            return;
+    $('.skills').each((i, element) => {
+        var name = $(element).data('name');
+        var supp = $(element).data('supp');
+        if (!name) {
+            alert('Champ skill: name non défini');
         }
 
-        newSkill(val);
-        e.target.value = '';
+        $(element).data('index', -1);
+
+        $(element).find('#ip_skills').keypress(e => {
+            if (e.key !== 'Enter') {
+                return;
+            }
+            e.preventDefault();
+
+            let i = e.target;
+            let val = i.value;
+            if (!val || !i.checkValidity()) {
+                return;
+            }
+
+            newSkill(element, val, name, supp);
+            e.target.value = '';
+        });
+
+        setItemDeletable($(element).find('.skills-item'));
+
+        var skills = $(element).data('skills');
+
+        if (skills) {
+            skills.forEach(h => newSkill(element, h, name, supp));
+        }
+
     });
-
-    setItemDeletable('.skills-item');
-
-    var skills = $('.skills').data('skills');
-
-    if (skills) {
-        skills.forEach(h => newSkill(h));
-    }
 }
 
-function newSkill(value) {
-    GLOBALS.skill_index++;
+function newSkill(element, value, name, supp) {
+    let index = $(element).data('index') + 1;
+    $(element).data('index', index);
+
     let hobby = `<span class="skills-item tag deletable onhover">
-		${value}<button class="delete mini-but glyphicon glyphicon-remove"><span class="sr-only sr-only-focusable">Supprimer la compétence</span></button>
-		<input type="hidden" name="fos_user_${GLOBALS.form}_form[skills][${GLOBALS.skill_index}][name]" value="${value}"/>
+		${value}<button class="delete mini-but glyphicon glyphicon-remove"><span class="sr-only sr-only-focusable">${supp}</span></button>
+		<input type="hidden" name="${GLOBALS.form}[${name}][${index}][name]" value="${value}"/>
 	</span>`;
 
-    $('.skills .skills-content .skills-add').after(hobby);
+    $(element).find('.skills-content .skills-add').after(hobby);
 
-    setItemDeletable($('.skills .skills-content .skills-item').first());
+    setItemDeletable($(element).find('.skills-content .skills-item').first());
 }
 
 //Hobbys
-
-function initHobby() {
-
-    $('#ip_hobbys').keypress(e => {
-        if (e.key !== 'Enter') {
-            return;
-        }
-        e.preventDefault();
-
-        let i = e.target;
-        let val = i.value;
-        if (!val || !i.checkValidity()) {
-            return;
-        }
-
-        newHobby(val);
-        e.target.value = '';
-    });
-
-    setItemDeletable('.hobbys-item');
-
-    var hobbys = $('.hobbys').data('hobbys');
-
-    if (hobbys) {
-        hobbys.forEach(h => newHobby(h));
-    }
-}
-
-function newHobby(value) {
-    GLOBALS.hobby_index++;
-    let hobby = `<span class="hobbys-item tag deletable onhover">
-		${value}<button class="delete mini-but glyphicon glyphicon-remove"><span class="sr-only sr-only-focusable">Supprimer le passe-temps</span></button>
-		<input type="hidden" name="fos_user_${GLOBALS.form}_form[hobbies][${GLOBALS.hobby_index}][name]" value="${value}"/>
-	</span>`;
-
-    $('.hobbys .hobbys-content .hobbys-add').after(hobby);
-
-    setItemDeletable($('.hobbys .hobbys-content .hobbys-item').first());
-}
+//
+//function initHobby() {
+//
+//    $('#ip_hobbys').keypress(e => {
+//        if (e.key !== 'Enter') {
+//            return;
+//        }
+//        e.preventDefault();
+//
+//        let i = e.target;
+//        let val = i.value;
+//        if (!val || !i.checkValidity()) {
+//            return;
+//        }
+//
+//        newHobby(val);
+//        e.target.value = '';
+//    });
+//
+//    setItemDeletable('.hobbys-item');
+//
+//    var hobbys = $('.hobbys').data('hobbys');
+//
+//    if (hobbys) {
+//        hobbys.forEach(h => newHobby(h));
+//    }
+//}
+//
+//function newHobby(value) {
+//    GLOBALS.hobby_index++;
+//    let hobby = `<span class="hobbys-item tag deletable onhover">
+//		${value}<button class="delete mini-but glyphicon glyphicon-remove"><span class="sr-only sr-only-focusable">Supprimer le passe-temps</span></button>
+//		<input type="hidden" name="${GLOBALS.form}[hobbies][${GLOBALS.hobby_index}][name]" value="${value}"/>
+//	</span>`;
+//
+//    $('.hobbys .hobbys-content .hobbys-add').after(hobby);
+//
+//    setItemDeletable($('.hobbys .hobbys-content .hobbys-item').first());
+//}
 
 //Educations
 
@@ -172,10 +191,10 @@ function newEducation(array) {
     }
 
     if (array.school) {
-        $('#fos_user_' + GLOBALS.form + '_form_educations_' + GLOBALS.education_index + '_school').val(array.school);
+        $('#' + GLOBALS.form + '_educations_' + GLOBALS.education_index + '_school').val(array.school);
     }
     if (array.degree) {
-        $('#fos_user_' + GLOBALS.form + '_form_educations_' + GLOBALS.education_index + '_degree').val(array.degree);
+        $('#' + GLOBALS.form + '_educations_' + GLOBALS.education_index + '_degree').val(array.degree);
     }
 
     applyDate('educations', array.fromDate, 'fromDate', GLOBALS.education_index);
@@ -217,13 +236,13 @@ function newWork(array) {
     }
 
     if (array.jobTitle) {
-        $('#fos_user_' + GLOBALS.form + '_form_workExperiences_' + GLOBALS.work_index + '_jobTitle').val(array.jobTitle);
+        $('#' + GLOBALS.form + '_workExperiences_' + GLOBALS.work_index + '_jobTitle').val(array.jobTitle);
     }
     if (array.employer) {
-        $('#fos_user_' + GLOBALS.form + '_form_workExperiences_' + GLOBALS.work_index + '_employer').val(array.employer);
+        $('#' + GLOBALS.form + '_workExperiences_' + GLOBALS.work_index + '_employer').val(array.employer);
     }
     if (array.description) {
-        $('#fos_user_' + GLOBALS.form + '_form_workExperiences_' + GLOBALS.work_index + '_description').val(array.description);
+        $('#' + GLOBALS.form + '_workExperiences_' + GLOBALS.work_index + '_description').val(array.description);
     }
 
     applyDate('workExperiences', array.fromDate, 'fromDate', GLOBALS.work_index);
